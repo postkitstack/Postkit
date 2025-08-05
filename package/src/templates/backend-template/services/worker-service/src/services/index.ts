f2bdbaaf69c172b6eb2563557d8c531f4b546bd5
Supun Appri <supun@appritechnologies.com>
2025-08-06 00:19:55 +0530

    feay : remove package folder

--- package/src/templates/backend-template/services/worker-service/src/services/index.ts
+++
@@ -1,9 +0,0 @@
-const dateTimeService = require('./date-time');
-const databaseService = require('./database').default;
-const { DatabaseQueries } = require('./database');
-
-module.exports = {
-  dateTimeService,
-  databaseService,
-  DatabaseQueries
-};
